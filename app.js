be0aee843b93e8c34a02f08e9c1b35b206fd8df0
thewebalok <alokprakash.net@gmail.com>
2025-03-18 23:22:12 +0530

    Fix port binding

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error.ejs", { message });
 });
 
-const PORT = process.env.PORT || 8088;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
